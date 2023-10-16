3408a086cc0d6aca21457c4297ca3bde78f46f5e
TanakAiko <cherifmbaye02@gmail.com>
2023-10-16 14:05:42 +0000

    maj

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ git add $files
 echo "Enter the message for the commit"
 read message
 git config --global user.email cherifmbaye02@gmail.com
-git config --global user.name cheimbaye
+git config --global user.name TanakAiko
 git commit -m "$message"
 git push
